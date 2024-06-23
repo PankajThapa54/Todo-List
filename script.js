66808a384d48e3402cf14de3665585bb7a0d5734
Pankaj Thapa <thapa54.pankaj@gmail.com>
2024-06-23 15:40:50 +0545

    Elements Added in HTML using DOM

--- script.js
+++ script.js
@@ -4,5 +4,34 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
+todoButton.addEventListener("click", addTodo);
 
 //Functions
+function addTodo(event) {
+  // Prevent form from submitting
+  event.preventDefault();
+  // Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //Check Mark Button
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-button");
+  todoDiv.appendChild(completeButton);
+
+  //Check trash Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-button");
+  todoDiv.appendChild(trashButton);
+
+  //Append to List
+  todoList.appendChild(todoDiv);
+}
